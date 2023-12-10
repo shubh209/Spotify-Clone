64a8093117995a65ac4d1b2d0d7665e8dc8e3297
shubhkapadia2031@gmail.com <Shubh.kce20@sot.pdpu.ac.in>
2023-12-10 20:21:59 +0530

    Frontend Done

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,10 +1,69 @@
-// import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Components/Player.js";
+import { getTokenFromResponse } from "./Components/Spotify.js";
+import "./App.css";   
+import Login from "./Components/Login.js";
+
+const s = new SpotifyWebApi();
 
 function App() {
+  const [{ token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      s.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      s.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      s.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: s,
+      });
+
+      s.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      s.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
+
   return (
     <div className="app">
-      <p>Hello World</p>
+      {!token && <Login />}
+      {token && <Player spotify={s} />}
     </div>
   );
 }
---
+++ spotify-clone/src/Components/Spotify.js
@@ -0,0 +1,29 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// client ID, redirect URI and desired scopes
+const clientId = "a063ab8916c24d41b3f715295471520f";
+const redirectUri = "https://localhost:3000/";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromResponse = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const accessUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
